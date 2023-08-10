35036f0c4497385427427631bfa6a2db01666498
direwolf2329 <140285894+direwolf2329@users.noreply.github.com>
2023-08-09 22:00:14 -0700

    Update sendemail.js

--- sendemail.js
+++ sendemail.js
@@ -5,31 +5,31 @@ const comments = document.getElementById('comments').value;
 const method = document.getElementById('method').value;
 const offer = document.getElementById('offer').value;
 
-let errors = "";
+let errors = '';
 
 if (!email.match(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/)) {
-  errors += "Invalid email address.\n";
+  errors += 'Invalid email address.\n';
 }
 
 if (!phone.match(/^\d{3}-\d{3}-\d{4}$/)) {
-  errors += "Invalid phone number.\n";
+  errors += 'Invalid phone number.\n';
 }
 
-if (comments.trim() === "") {
-  errors += "Please enter comments.\n";
+if (comments.trim() === '') {
+  errors += 'Please enter comments.\n';
 }
 
-if (errors === "") {
-  const to = "cecillepapa@student.purdueglobal.edu";
-  const subject = `Contact Form Submission from ${name} for EZ Shop`;
-  let message = "You have received a new message from your contact form.\n\n";
-  message += "Here are the details:\n\n";
-  message += `Name: ${name}\n`;
-  message += `Email: ${email}\n`;
-  message += `Phone: ${phone}\n`;
-  message += `Comments: ${comments}\n`;
-  message += `Preferred Contact Method: ${method}\n`;
-  message += `Special Offer: ${offer}\n`;
+if (errors === '') {
+  const to = 'cecillepapa@student.purdueglobal.edu';
+  const subject = 'Contact Form Submission from ${name} for EZ Shop';
+  let message = 'You have received a new message from your contact form.\n\n';
+  message += 'Here are the details:\n\n';
+  message += 'Name: ${name}\n';
+  message += 'Email: ${email}\n';
+  message += 'Phone: ${phone}\n';
+  message += 'Comments: ${comments}\n';
+  message += 'Preferred Contact Method: ${method}\n';
+  message += 'Special Offer: ${offer}\n';
 
   // Send email
   // ...
