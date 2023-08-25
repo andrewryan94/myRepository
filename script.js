6e513e7d38fd0208ef06fb9c11ee37436a8def92
direwolf2329 <140285894+direwolf2329@users.noreply.github.com>
2023-08-24 19:49:29 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+<script>
+	var cartContainer = document.querySelector(“.cart-container”); 
+	var cartItems = document.querySelectorAll(“.cart-item”); 
+	var clearCartButton = document.getElementById(“clear-cart”); 
+	var checkoutButton = document.getElementById(“check out”); 
+	var subtotalElement = document.querySelector(“.cart-item-subtotal”);
+
+	function calculateTotalPrice() { var totalPrice = 0; 
+
+	for (var i = 0; i < cartItems.length; i++) { var cartItem = cartItems[i]; 
+	var priceElement = cartItem.querySelector(“.cart-item-price”); 
+	var quantityElement = cartItem.querySelector(“.quantity”); 
+						    
+	var price = parseFloat(priceElement.innerText.replace(“$”, “”)); 
+	var quantity = parseInt(quantityElement.value); 
+						    
+	totalPrice += price * quantity; } 
+					
+	return totalPrice.toFixed(2); }
+
+	function updateSubtotal() { var totalPrice = calculateTotalPrice();
+	subtotalElement.innerText = “$” + totalPrice; }
+
+	function removeCartItem(event) { 
+		
+	var buttonClicked = event.target; 
+		
+	var cartItem = buttonClicked.parentElement; 
+		
+	cartContainer.removeChild(cartItem); 
+		
+	updateSubtotal(); }
+
+	function clearCart() { 
+
+	for (var i = 0; i < cartItems.length; i++) { var cartItem = cartItems[i];
+	cartContainer.removeChild(cartItem); } 
+		
+	updateSubtotal(); }
+
+	clearCartButton.addEventListener(“click”, clearCart);
+
+	for (var i = 0; i < cartItems.length; i++) { var cartItem = cartItems[i];
+	var removeButton = cartItem.querySelector(“.cart-item-remove”);
+	removeButton.addEventListener(“click”, removeCartItem); }
+
+	for (var i = 0; i < cartItems.length; i++) { var cartItem = cartItems[i];
+	var quantityInput = cartItem.querySelector(“.quantity”); 
+	quantityInput.addEventListener(“change”, updateSubtotal); }
+
+	updateSubtotal();
+</script>
